fa7cdd500e6c5d91e95fca038b482a88f8a9a3b2
Jackson <jl14u21@soton.ac.uk>
2023-11-21 16:23:36 +0000

    Added Blue slime, no movement anim yet

--- Assets/_Scripts/Enemy/Enemy.cs
+++ Assets/_Scripts/Enemy/Enemy.cs
@@ -4,15 +4,40 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+
+    public int maxHealth = 100;
+    private int currentHealth;
+
+    public Animator animator;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        currentHealth = maxHealth;
     }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
+    // Call from other scripts to hurt enemy.
+    public void TakeDamage(int damage) {
+        currentHealth -= damage;
+
+        //Play hurt animation
+        animator.SetTrigger("Hurt");
+
+        if(currentHealth <= 0) {
+
+            Die();
+
+        }
+
     }
+
+    void Die() {
+        //Play die animation
+        animator.SetBool("IsDead", true);
+
+        //Disable the enemy
+        GetComponent<Collider2D>().enabled = false;
+        this.enabled = false;
+    }
+
 }
