1a757cf7ea1aee561bfa5d0dc5c01aab56f17e72
Jackson <jl14u21@soton.ac.uk>
2023-12-16 16:47:39 +0200

    Fixed bug and adjusted strawberry

--- Assets/_Scripts/Props/Strawberry.cs
+++ Assets/_Scripts/Props/Strawberry.cs
@@ -25,19 +25,32 @@ public class Strawberry : MonoBehaviour
             AudioManager.Instance.PlayOneShot("Collect");
 
 
-            StartCoroutine(DestroyAfterSeconds(1));
+            //StartCoroutine(DestroyAfterSeconds(1));
 
         }
 
     }
 
-    IEnumerator DestroyAfterSeconds(int seconds)
-    {
-        // Wait for the specified number of seconds
-        yield return new WaitForSeconds(seconds);
-
-        // Destroy the GameObject this script is attached to
-        Destroy(gameObject);
+    private void Update() {
+        // Check if the animation is playing
+        if (animator.GetCurrentAnimatorStateInfo(0).IsName("Collected"))
+        {
+            // Check if the animation is finished
+            if (animator.GetCurrentAnimatorStateInfo(0).normalizedTime >= 1.0f)
+            {
+                // Delete the object
+                Destroy(gameObject);
+            }
+        }
     }
+
+    // IEnumerator DestroyAfterSeconds(int seconds)
+    // {
+    //     // Wait for the specified number of seconds
+    //     yield return new WaitForSeconds(seconds);
+
+    //     // Destroy the GameObject this script is attached to
+    //     Destroy(gameObject);
+    // }
 
 }
